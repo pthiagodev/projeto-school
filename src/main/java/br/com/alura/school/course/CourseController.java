ed9d5d0bda4dad736325b862afae5517806d5001
Thiago de Assis <paulo.antunes@meta.com.br>
2023-02-23 07:06:46 -0300

    fix: altera método para retornar lista de CourseResponse.

--- src/main/java/br/com/alura/school/course/CourseController.java
+++ src/main/java/br/com/alura/school/course/CourseController.java
@@ -7,6 +7,8 @@ import org.springframework.web.server.ResponseStatusException;
 import javax.validation.Valid;
 import java.net.URI;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static java.lang.String.format;
 import static org.springframework.http.HttpStatus.NOT_FOUND;
@@ -22,7 +24,8 @@ class CourseController {
 
     @GetMapping("/courses")
     ResponseEntity<List<CourseResponse>> allCourses() {
-        return ResponseEntity.ok().build();
+        List<CourseResponse> courses = courseRepository.findAll().stream().map(CourseResponse::new).collect(Collectors.toList());
+        return ResponseEntity.ok(courses);
     }
 
     @GetMapping("/courses/{code}")
