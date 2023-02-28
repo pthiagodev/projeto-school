05ddce279eb4a69a9c69265ed8a49a7df70ccb8e
Thiago de Assis <paulo.antunes@meta.com.br>
2023-02-28 17:24:32 -0300

    feat: adiciona repositório para matrículas.

---
+++ src/main/java/br/com/alura/school/enrollment/EnrollmentRepository.java
@@ -0,0 +1,6 @@
+package br.com.alura.school.enrollment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+}
