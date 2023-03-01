c6006b4dc30bfbbd7c6576c4f0a62f9e200a21e1
Thiago de Assis <paulo.antunes@meta.com.br>
2023-03-01 12:30:56 -0300

    feat: cria DTO de Response para relatório de matrículas.

--- src/main/java/br/com/alura/school/enrollment/NewEnrollmentRequest.java
+++ src/main/java/br/com/alura/school/enrollment/NewEnrollmentRequest.java
@@ -1,4 +1,31 @@
 package br.com.alura.school.enrollment;
 
+import br.com.alura.school.course.Course;
+import br.com.alura.school.support.validation.Unique;
+import br.com.alura.school.user.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+
 public class NewEnrollmentRequest {
+
+    @Size(max=20)
+    @NotBlank
+    @JsonProperty("username")
+    private String username;
+
+    NewEnrollmentRequest(){}
+    NewEnrollmentRequest(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    Enrollment toEntity(User user, Course course) {
+        return new Enrollment(user, course);
+    }
 }
