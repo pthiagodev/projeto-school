dc242d5c5b2d0fb4772d84d1e5b516048fa406c8
Thiago de Assis <paulo.antunes@meta.com.br>
2023-03-01 17:00:19 -0300

    refactor: adiciona método que busca matricula existente para o usuário.

--- src/main/java/br/com/alura/school/enrollment/EnrollmentRepository.java
+++ src/main/java/br/com/alura/school/enrollment/EnrollmentRepository.java
@@ -1,6 +1,13 @@
 package br.com.alura.school.enrollment;
 
+import br.com.alura.school.course.Course;
+import br.com.alura.school.user.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
+
+    Optional<Enrollment> findEnrollmentByUserAndCourse(User user, Course course);
+
 }
